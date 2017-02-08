88ec42b70fd0c52792161e90739544077a595c34
laozhaochaguan@hotmail.com <laozhaochaguan@gmail.com>
2017-02-08 21:18:33 +0800

    update

--- views/login/index.js
+++ views/login/index.js
@@ -7,8 +7,6 @@ $(document).ready(function() {
   $('.page-header').on("click", function() {
     dialog('测试一下啦', function () {
       dialog('6666', null, 3000);
-      notyf.alert('You must fill out the form before moving forward');
     });
   });
-  notyf.confirm('Your changes have been successfully saved!');
 });
