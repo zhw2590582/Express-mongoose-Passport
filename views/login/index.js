a20fb7ae8d631897f64b6df7b34436452639e873
laozhaochaguan@hotmail.com <laozhaochaguan@gmail.com>
2017-02-08 21:40:46 +0800

    update

--- views/login/index.js
+++ views/login/index.js
@@ -1,9 +1,11 @@
 var dialog = require('../common/dialog');
+var parsley = require('../common/parsley');
 
 require('spectre.css');
 require('./index.scss');
 
 $(document).ready(function() {
+  console.log(parsley);
   $('.page-header').on("click", function() {
     dialog('测试一下啦', function () {
       dialog('6666', null, 3000);
