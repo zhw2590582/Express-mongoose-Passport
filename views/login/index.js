36e146260aea3c13c3729cd8f0379deb4b511d05
laozhaochaguan@hotmail.com <laozhaochaguan@gmail.com>
2017-02-08 23:02:36 +0800

    update

--- views/login/index.js
+++ views/login/index.js
@@ -6,11 +6,13 @@ require('../common/style/base.scss');
 require('./index.scss');
 
 $(document).ready(function() {
-  $('.page-header').on("click", function() {
-    dialog('测试一下啦', function() {
-      dialog('6666', null, 3000);
-    });
-  });
+
+  //隐藏服务器消息
+  setTimeout(function() {
+    $('.warning-info').slideToggle();
+  }, 3000);
+
+  //dialog('6666', null, 3000);
 
   $('#login-form').parsley().on('field:validated', function() {
       var ok = $('.parsley-error').length === 0;
